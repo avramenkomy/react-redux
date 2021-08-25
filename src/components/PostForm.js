301501c4a2274aa6b613ecf61db535f258588a4b
avramenkomy <avramenkomy@yandex.ru>
2021-08-25 13:40:51 +0300

    WIP: add handlers for input and set new state

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -4,18 +4,40 @@ export default class PostForm extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      title: '',
+    };
   }
 
   submitHandler = event => {
     event.preventDefault();
+
+    const {title} = this.state;
+    const newPost = { title, id: Date.now().toString() }
+
+    console.log(newPost);
   }
+
+  changeInputHandler = event => {
+    event.persist();
+    this.setState(prev => ({...prev, ...{
+      [event.target.name]: event.target.value
+      }}))
+  }
+
   render() {
     return (
       <form onSubmit={this.submitHandler}>
         <div className="form-group">
           <label htmlFor="title">Заголовок поста</label>
-          <input type="text" className="form-control" id="title" />
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            value={this.state.title}
+            onChange={this.changeInputHandler}
+            name="title"
+          />
         </div>
         <button className="btn btn-success mt-1" type="submit">Создать</button>
       </form>
