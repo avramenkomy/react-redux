2baaa969ca2963ca2d43edd0fbf29aa9c57d1f31
avramenkomy <avramenkomy@yandex.ru>
2021-08-25 17:09:48 +0300

    WIP: add check for empty post

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -15,6 +15,10 @@ class PostForm extends React.Component {
     event.preventDefault();
 
     const {title} = this.state;
+    if (!title.trim()) {
+      return
+    }
+
     const newPost = { title, id: Date.now().toString() }
 
     this.props.createPost(newPost);
