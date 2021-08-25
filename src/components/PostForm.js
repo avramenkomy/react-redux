a1ddd31c6d3c89e3f595f671a8c08dad7f9985fc
avramenkomy <avramenkomy@yandex.ru>
2021-08-25 13:49:12 +0300

    WIP: add clear input field after submit

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -16,6 +16,8 @@ export default class PostForm extends React.Component {
     const newPost = { title, id: Date.now().toString() }
 
     console.log(newPost);
+
+    this.setState({ title: '' });
   }
 
   changeInputHandler = event => {
