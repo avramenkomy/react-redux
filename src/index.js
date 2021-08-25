76907a65a98503484d656eb3f828809c5b4817b7
avramenkomy <avramenkomy@yandex.ru>
2021-08-25 22:53:25 +0300

    WIP: add example work with custom middleware

--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ import {Provider} from 'react-redux';
 import thunk from "redux-thunk";
 import App from "./App";
 import {rootReducer} from "./redux/rootReducer";
+import {forbiddenWordsMiddleware} from "./redux/middleware";
 
 const store = createStore(rootReducer, compose(
   applyMiddleware(
-    thunk
+    thunk, forbiddenWordsMiddleware
   ),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ));
