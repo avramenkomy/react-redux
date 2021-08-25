ae8a0193019d3a05046af55d4198a3c0fb208ee1
avramenkomy <avramenkomy@yandex.ru>
2021-08-25 10:21:45 +0300

    WIP: add initial layout App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,20 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="container">
-      <h1>React</h1>
+    <div className="container pt-3">
+      <div className="row">
+          <div className="col">
+              FORM
+          </div>
+      </div>
+      <div className="row">
+          <div className="col">
+              POSTS
+          </div>
+          <div className="col">
+              FETCHED POSTS
+          </div>
+      </div>
     </div>
   );
 }
