ea9f112b1a06f76ad40a3e76b4aaf1e92afdb602
avramenkomy <avramenkomy@yandex.ru>
2021-08-25 15:33:25 +0300

    WIP: connect redux devtools

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import {render} from 'react-dom';
 import reportWebVitals from './reportWebVitals';
-import {createStore} from 'redux';
+import {createStore, compose} from 'redux';
+import {Provider} from 'react-redux';
 import App from "./App";
 import {rootReducer} from "./redux/rootReducer";
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+));
 
+const app =(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
 render(
   <React.StrictMode>
-    <App />
+    {app}
   </React.StrictMode>,
   document.getElementById('root')
 );
